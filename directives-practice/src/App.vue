6fd7bfd77538468b2358b4c072fd60b7057060ba
hirouniverse <hiro.kikuchi.universe@gmail.com>
2020-05-12 21:27:31 +0700

    add: directives practice

---
+++ directives-practice/src/App.vue
@@ -0,0 +1,46 @@
+<template>
+    <div class="container">
+        <div class="row">
+            <div class="col-xs-12 col-sm-8 col-sm-offset-2 col-md-6 col-md-offset-3">
+                <h1>Directives Exercise</h1>
+                <!-- Exercise -->
+                <!-- Build a Custom Directive which works like v-on (Listen for Events) -->
+                <button v-myon:click="click">click me</button>
+
+                <div
+                    style="width: 100px; height: 100px; background-color: lightgreen;"
+                    v-myon:mouseenter="mouseEnter"
+                    v-myon:mouseleave="mouseLeft"></div>
+
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+    export default {
+        directives: {
+            'myon': {
+                bind(el, binding, vnode) {
+                    let type = binding.arg;
+                    let fn = binding.value;
+                    el.addEventListener(type, fn);
+                }
+            }
+        },
+        methods: {
+            click() {
+                alert('This is clicked');
+            },
+            mouseEnter() {
+                console.log('enter!');
+            },
+            mouseLeft() {
+                console.log('left!');
+            }
+        }
+    }
+</script>
+
+<style>
+</style>
