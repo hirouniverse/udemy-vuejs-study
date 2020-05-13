2634601b4b1695e2dde4b06415a83a4dc1392991
hirouniverse <hiro.kikuchi.universe@gmail.com>
2020-05-13 18:06:19 +0700

    refactor: vue-router setting file

--- stock-trader/src/main.js
+++ stock-trader/src/main.js
@@ -1,9 +1,17 @@
 import Vue from 'vue'
+import VueRouter from 'vue-router'
 import App from './App.vue'
 
+import routes from './routes/';
 
 // vue-router setting
-import router from './routes/';
+Vue.use(VueRouter);
+const router = new VueRouter({
+  mode: 'history',
+  routes,
+});
+
+
 new Vue({
   el: '#app',
   router,
