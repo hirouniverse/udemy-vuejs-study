e23c0c53435b863bc8cdc57cd72c2ee47dfba8e3
hirouniverse <hiro.kikuchi.universe@gmail.com>
2020-05-13 18:09:59 +0700

    add: stock page components

--- stock-trader/src/components/stocks/Stock.vue
+++ stock-trader/src/components/stocks/Stock.vue
@@ -1,5 +1,20 @@
 <template>
-    <div>
-        The Stocks - Stock Component
+    <div class="col-sm-6 col-md-4">
+        <div class="card text-black bg-light mb-3">
+            <div class="card-header">
+                <slot name="id"></slot>
+            </div>
+            <div class="card-body">
+                <h5 class="card-title">
+                    <slot name="title"></slot>
+                </h5>
+                <p class="card-text">
+                    <slot name="content"></slot>
+                </p>
+            </div>
+        </div>
     </div>
 </template>
+
+<style>
+</style>
--- stock-trader/src/components/stocks/Stocks.vue
+++ stock-trader/src/components/stocks/Stocks.vue
@@ -1,5 +1,29 @@
 <template>
-    <div>
-        The Stocks Component
+    <div class="row" style="padding: 10px;">
+        <app-stock v-for="stock in stocks" :key="stock.id">
+            <template slot="id">{{ stock.id }}</template>
+            <template slot="title">{{ stock.name }}</template>
+            <template slot="content">{{ stock.price }}</template>
+        </app-stock>
     </div>
 </template>
+
+<script>
+import Stock from './Stock.vue';
+export default {
+    data() {
+        return {
+            stocks: [
+                { id: 1, name: 'BMW', price: 110 },
+                { id: 2, name: 'Google', price: 200 },
+                { id: 3, name: 'Apple', price: 250 },
+                { id: 4, name: 'Twitter', price: 100 },
+                { id: 5, name: 'Mercari', price: 60 },
+            ],
+        }
+    },
+    components: {
+        appStock: Stock
+    }
+}
+</script>
