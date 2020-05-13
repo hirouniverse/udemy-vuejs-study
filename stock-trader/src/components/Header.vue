12ef743c891be4a45032f0cbfc6ceb3492515abf
hirouniverse <hiro.kikuchi.universe@gmail.com>
2020-05-13 18:08:53 +0700

    refactor: header menu

--- stock-trader/src/components/Header.vue
+++ stock-trader/src/components/Header.vue
@@ -3,27 +3,39 @@
         <header class="col-xs-12 col-sm-12 col-md-12">
             <nav class="navbar navbar-expand-lg navbar-light bg-light">
                 <router-link to="/">
-                    <a class="navbar-brand" href="#">
+                    <a class="navbar-brand">
                         <img src="../assets/logo.png" width="30" height="30" class="d-inline-block align-top" alt="">
                         Stock Trader
                     </a>
                 </router-link>
                 <div class="collapse navbar-collapse" id="navbarText">
                     <ul class="navbar-nav mr-auto">
-                        <li class="nav-item active">
-                            <router-link to="/portfolio">
-                                <a class="nav-link" href="#">Portfolio</a>
-                            </router-link>
-                        </li>
-                        <li class="nav-item">
-                            <router-link to="/stocks">
-                                <a class="nav-link" href="#">Stocks</a>
-                            </router-link>
+                        <router-link to="/portfolio" class="nav-item" activeClass="active" tag="li">
+                            <a class="nav-link">Portfolio</a>
+                        </router-link>
+                        <router-link to="/stocks" class="nav-item" activeClass="active" tag="li">
+                            <a class="nav-link">Stocks</a>
+                        </router-link>
+                    </ul>
+                    <ul class="navbar-nav">
+                        <li class="nav-item"><a class="nav-link">End Day</a></li>
+                        <li class="nav-item dropdown">
+                            <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                                Dropdown
+                            </a>
+                            <div class="dropdown-menu" aria-labelledby="navbarDropdown">
+                                <a class="dropdown-item" href="#">Action</a>
+                                <a class="dropdown-item" href="#">Another action</a>
+                                <div class="dropdown-divider"></div>
+                                <a class="dropdown-item" href="#">Something else here</a>
+                            </div>
                         </li>
                     </ul>
-                    <span class="navbar-text">
+                    <!--
+                    <span class="navbar-text" style="margin-left: 30px;">
                         Fund: ${{ saving }}
                     </span>
+                    -->
                 </div>
             </nav>
         </header>
