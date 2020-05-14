309406acffe5bb997998e2e39c52ad737c62b9d1
hirouniverse <hiro.kikuchi.universe@gmail.com>
2020-05-14 20:49:02 +0700

    add: init state logic

--- stock-trader/src/components/Home.vue
+++ stock-trader/src/components/Home.vue
@@ -1,15 +1,21 @@
 <template>
-    <div>
-        <p>The Home Component</p>
+    <div class="row" style="padding: 10px;">
+        <div class="col-12 col-sm-12 col-md-12">
+            <h1>Trade or View your Portfolio</h1>
+            <h6>You may Save & Load your Data</h6>
+            <h6>Click on 'End Day' to begin a New Day!</h6>
+            <hr>
+            <strong>Your Funds: {{ funds | currency }}</strong>
+        </div>
     </div>
 </template>
 
 <script>
     export default {
-        data() {
-            return {
-                saving: 900,
+        computed: {
+            funds() {
+                return this.$store.getters.myFunds;
             }
-        },
+        }
     }
 </script>
