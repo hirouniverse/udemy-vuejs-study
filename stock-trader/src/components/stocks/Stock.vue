7941ae4e6efe2b2a582212c9d9fb2603e110ae91
hirouniverse <hiro.kikuchi.universe@gmail.com>
2020-05-14 20:49:37 +0700

    add: stock components

--- stock-trader/src/components/stocks/Stock.vue
+++ stock-trader/src/components/stocks/Stock.vue
@@ -1,16 +1,36 @@
 <template>
     <div class="col-sm-6 col-md-4">
-        <div class="card text-black bg-light mb-3">
-            <div class="card-header">
-                <slot name="id"></slot>
-            </div>
-            <div class="card-body">
-                <h5 class="card-title">
-                    <slot name="title"></slot>
+        <div class="card mb-3">
+            <div class="card-header bg-primary text-white font-weight-bold">
+                <h5>
+                    {{ stock.name }}
+                    <small>( Price: {{ stock.price }} )</small>
                 </h5>
-                <p class="card-text">
-                    <slot name="content"></slot>
-                </p>
+            </div>
+            <div class="card-body bg-light text-black">
+                <div class="row">
+                    <div class="col-6 col-sm-6 col-md-6">
+                        <input
+                            type="number"
+                            min="0"
+                            class="form-control"
+                            :class="{ 'border': insufficientFunds, 'border-danger': insufficientFunds }"
+                            placeholder="Quantity"
+                            v-model.number="quantity">
+                    </div>
+                    <div class="col-3 offset-3 col-sm-3 offset-sm-2 col-md-3">
+                        <button
+                            class="btn btn-success"
+                            :disabled="( insufficientFunds || quantity <= 0 || !Number.isInteger(quantity))"
+                            @click="buy">
+                                {{ insufficientFunds ? 'No Enough Funds' : 'Buy'}}
+                        </button>  
+                    </div>
+                </div>
+                <div class="float-left">
+                </div>
+                <div class="float-right">  
+                </div>
             </div>
         </div>
     </div>
@@ -18,3 +38,48 @@
 
 <style>
 </style>
+
+<script>
+export default {
+    props: {
+        stock: {
+            type: Object,
+            default: {
+                id: 'unknown',
+                name: 'unknown',
+                price: 0,
+            }
+        },
+    },
+    data() {
+        return {
+            quantity: 0,
+        }
+    },
+    computed: {
+        funds() {
+            return this.$store.getters.myFunds;
+        },
+        insufficientFunds() {
+            return this.quantity * this.stock.price > this.funds;
+        }
+    },
+    methods: {
+        buy() {
+            if (this.quantity <= 0) {
+                alert('please input valid quantity.');
+                return;
+            }
+            const order = {
+                stockId: this.stock.id,
+                stockPrice: this.stock.price,
+                quantity: this.quantity,
+            };
+            
+            console.log(order);
+            this.$store.dispatch('buyStock', order);
+            this.quantity = 0;
+        }
+    }
+}
+</script>
--- stock-trader/src/components/stocks/Stocks.vue
+++ stock-trader/src/components/stocks/Stocks.vue
@@ -1,9 +1,6 @@
 <template>
     <div class="row" style="padding: 10px;">
-        <app-stock v-for="stock in stocks" :key="stock.id">
-            <template slot="id">{{ stock.id }}</template>
-            <template slot="title">{{ stock.name }}</template>
-            <template slot="content">{{ stock.price }}</template>
+        <app-stock v-for="stock in stocks" :key="stock.id" :stock="stock">
         </app-stock>
     </div>
 </template>
@@ -11,19 +8,13 @@
 <script>
 import Stock from './Stock.vue';
 export default {
-    data() {
-        return {
-            stocks: [
-                { id: 1, name: 'BMW', price: 110 },
-                { id: 2, name: 'Google', price: 200 },
-                { id: 3, name: 'Apple', price: 250 },
-                { id: 4, name: 'Twitter', price: 100 },
-                { id: 5, name: 'Mercari', price: 60 },
-            ],
-        }
-    },
     components: {
         appStock: Stock
-    }
+    },
+    computed: {
+        stocks() {
+            return this.$store.getters.stocks;
+        }
+    },
 }
 </script>
